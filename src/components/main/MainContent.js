f928b35815bdbc0984c9d88ac5fe68d2807e5cb0
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-03 23:50:16 +0330

    navbar added for small size

--- src/components/main/MainContent.js
+++ src/components/main/MainContent.js
@@ -1,31 +1,34 @@
-import { Button, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import { useContext, useLayoutEffect } from 'react';
 import TabPanel from './TabPanel';
 import DataContext from '../../contexts/DataContext';
 import DrawerTabs from '../drawer/DrawerTabs';
+import Navbar from './Navbar';
 
 const MainContent = () => {
-    const { value, open, setOpen } = useContext(DataContext);
+    const { value, open } = useContext(DataContext);
     useLayoutEffect(() => {
         document.title = 'Personal App';
     }, [])
     return (
-        <Stack alignItems={'center'} display={'flex'} >
-            <TabPanel value={value} index={0}>
-                Item 1
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Item 2
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                Item 3
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                Item 4
-            </TabPanel>
-            <Button sx={{ display: { xs: 'block', sm: 'block', md: 'none' } }} onClick={() => setOpen(true)}>click</Button>
+        <>
+            <Navbar />
             <DrawerTabs drawerOpen={open} />
-        </Stack>
+            <Stack alignItems={'center'} display={'flex'} >
+                <TabPanel value={value} index={0}>
+                    Item 1
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item 2
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item 3
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                    Item 4
+                </TabPanel>
+            </Stack>
+        </>
     );
 }
 
