8c69736938b0aad5ba5b2857de2e031f6f497f63
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-04 02:36:15 +0330

    auth layout deleted

--- src/routs.js
+++ src/routs.js
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainContent from "./components/main/MainContent";
-import AuthLayout from "./layouts/AuthLayout";
 import MainLayout from "./layouts/MainLayout";
 import NotFound from "./pages/NotFound";
 
@@ -9,9 +8,6 @@ export const routs = createBrowserRouter([
         path: '/', element: <MainLayout />, children: [
             { path: '/', element: <MainContent /> },
         ]
-    },
-    {
-        path: '/auth', element: <AuthLayout />, children: []
     },
     { path: '*', element: <NotFound /> }
 ])
