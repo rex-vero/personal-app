9d5264806653e38e0c274be1fd24e04c5f8209f1
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-04 02:29:46 +0330

    helpers added

--- src/components/main/MainContent.js
+++ src/components/main/MainContent.js
@@ -1,6 +1,6 @@
 import { Stack } from '@mui/material';
 import { useContext, useLayoutEffect } from 'react';
-import TabPanel from './TabPanel';
+import TabPanel from '../../helpers/TabPanel';
 import DataContext from '../../contexts/DataContext';
 import DrawerTabs from '../drawer/DrawerTabs';
 import Navbar from './Navbar';
