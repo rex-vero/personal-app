80f94717fcced72b4220a798b49de75e20b1c49f
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-04 19:28:28 +0330

    tabpanel style editted

--- src/helpers/TabPanel.js
+++ src/helpers/TabPanel.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 const TabPanel = ({ children, value, index, ...other }) => {
     return (
         <div role="tabpanel" hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tabpanel-${index}`} {...other}>
-            {value === index && <Box height={'100vh'} display={'flex'} justifyContent={'center'} alignItems={'center'}>{children}</Box>}
+            {value === index && <Box>{children}</Box>}
         </div>
     );
 }
