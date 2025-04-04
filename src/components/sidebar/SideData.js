1f654361d2ebb2cee7f0015596a61aad8172ad30
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-04 17:35:29 +0330

    Tabs refactored

---
+++ src/components/sidebar/SideData.js
@@ -0,0 +1,18 @@
+import { Typography } from '@mui/material';
+import { IoHome, IoPerson } from "react-icons/io5";
+import { PiPaperclipLight } from 'react-icons/pi';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { grey } from '@mui/material/colors';
+
+const tabProps = index => ({
+    id: `tabpanel-${index}`,
+    value: index,
+    'aria-controls': `tabpanel-${index}`,
+    sx: { '&.MuiTab-root': { 'm': 1, 'borderRadius': 2, 'backgroundColor': { 'xs': grey[800], 'md': grey[500] } } }
+});
+export const tabsData = [
+    { label: <Typography fontSize={13}>Home</Typography>, icon: <IoHome fontSize={24} />, ...tabProps(0) },
+    { label: <Typography fontSize={13}>About Me</Typography>, icon: <IoPerson fontSize={24} />, ...tabProps(1) },
+    { label: <Typography fontSize={13}>Resume</Typography>, icon: <PiPaperclipLight fontSize={24} />, ...tabProps(2) },
+    { label: <Typography fontSize={13}>Contact Me</Typography>, icon: <FaPhoneAlt fontSize={24} />, ...tabProps(3) }
+];
--- src/components/sidebar/SideLinks.js
+++ src/components/sidebar/SideLinks.js
@@ -1,25 +1,13 @@
-import { Tabs, Tab, Typography } from '@mui/material';
+import { Tabs, Tab } from '@mui/material';
 import { useContext } from 'react';
-import { IoHome, IoPerson } from "react-icons/io5";
-import { PiPaperclipLight } from 'react-icons/pi';
 import DataContext from '../../contexts/DataContext';
-import { FaPhoneAlt } from 'react-icons/fa';
-import { grey } from '@mui/material/colors';
+import { tabsData } from './SideData';
 
 const SideLinks = () => {
     const { value, setValue } = useContext(DataContext);
-    const tabProps = index => ({
-        id: `tabpanel-${index}`,
-        value: index,
-        'aria-controls': `tabpanel-${index}`,
-        sx: { '&.MuiTab-root': { 'm': 1, 'borderRadius': 2, 'backgroundColor': { 'xs': grey[800], 'md': grey[500] } } }
-    });
     return (
         <Tabs textColor='secondary' value={value} onChange={(_, tabNewValue) => setValue(tabNewValue)} orientation='vertical' sx={{ mt: 5 }}>
-            <Tab label={<Typography fontSize={13}>Home</Typography>} icon={<IoHome fontSize={24} />} {...tabProps(0)} />
-            <Tab label={<Typography fontSize={13}>About Me</Typography>} icon={<IoPerson fontSize={24} />} {...tabProps(1)} />
-            <Tab label={<Typography fontSize={13}>Resume</Typography>} icon={<PiPaperclipLight fontSize={24} />} {...tabProps(2)} />
-            <Tab label={<Typography fontSize={13}>Contact Me</Typography>} icon={<FaPhoneAlt fontSize={24} />} {...tabProps(3)} />
+            {tabsData.map((item, key) => <Tab key={key} label={item.label} icon={item.label} {...item} />)}
         </Tabs>
     );
 }
