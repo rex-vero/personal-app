c9787e50f9ca74afdf2803349fb10f4d0bda5143
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-04 03:36:06 +0330

    animation added

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
-import { Grid } from "@mui/material";
+import { Grid, keyframes } from "@mui/material";
 import { grey } from "@mui/material/colors";
 
 const MainLayout = () => {
+    const blurAnimation = keyframes`
+  0% {
+    filter: blur(10px);
+  }
+  100% {
+    filter: blur(0);
+  }
+`;
     return (
-        <Grid container columns={12} sx={{ height: '100vh' }}>
+        <Grid container columns={12} sx={{ animation: `${blurAnimation} 1s ease-in-out`, height: '100vh' }}>
             <Grid bgcolor={grey[500]} size={{ xs: 0, sm: 0, md: 2 }} sx={{ display: { xs: 'none', md: 'block' } }}>
                 <Sidebar />
             </Grid>
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,40 @@
+import { Box, Button, Typography, keyframes } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { useLayoutEffect } from "react";
+import { TbArrowBackUp } from "react-icons/tb";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
+    const floatAnimation = keyframes`
+0% {
+  transform: translateY(0);
+}
+50% {
+  transform: translateY(-10px);
+}
+100% {
+  transform: translateY(0);
+}
+`;
+    useLayoutEffect(() => {
+        document.title = 'Page Not Found';
+        document.body.style.overflow = 'hidden';
+        return () => document.body.style.overflow = 'unset';
+    }, [])
     return (
-        <div>
-            <div>
-                <span>404</span>
-            </div>
-        </div>
+        <Box height={'100vh'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+            <Box boxShadow={'0 0 10px black'} borderRadius={4} display={'flex'} justifyContent={'center'} alignItems={'center'} sx={{ backgroundColor: grey[900], p: 3, m: 2, animation: `${floatAnimation} 2s ease-in-out infinite` }}>
+                <Box>
+                    <Typography variant="h3" color="textPrimary">OOPS 404 ERROR!</Typography>
+                    <Typography variant="h6" sx={{ mb: 3 }} color="textSecondary">We Couldn't Find The Page You Were Looking For</Typography>
+                    <Link to={'/'}>
+                        <Button variant="contained" sx={{ borderRadius: 4 }} endIcon={<TbArrowBackUp />}>
+                            Go Back
+                        </Button>
+                    </Link>
+                </Box>
+            </Box>
+        </Box>
     );
 }
 
