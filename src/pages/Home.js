70d276881adf25406ff2053a6815315fd2cc2601
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-06 18:40:22 +0330

    UI updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
     return (
         <Box textAlign={'center'} m={1}>
             <ParticlesContainer particleColor={blue[100]} animationType={'snow'} />
-            <Typography color={grey[50]} variant="h4">
+            <Typography color={grey[50]} sx={{ fontSize: { xs: '1.5rem', md: '3rem' } }}>
                 <Typing strings={['[{ "Mohammad Amin Ahmadi" }]']} typeSpeed={200} />
             </Typography>
-            <Typography variant="h6" color={green[800]}>
+            <Typography sx={{ fontSize: { xs: '1.2rem', md: '1.8rem' } }} color={green[800]}>
                 <Typing loop strings={["I'm Front-End Developer", "I'm Programer", "I'm Always Excited To Bring Life With Code"]} typeSpeed={90} backSpeed={25} />
             </Typography>
         </Box>
