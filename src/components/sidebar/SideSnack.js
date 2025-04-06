4be5556a2b03b48cf2545334e86e47542f006989
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-06 17:44:31 +0330

    snakbar fixed

--- src/components/sidebar/SideSnack.js
+++ src/components/sidebar/SideSnack.js
@@ -6,7 +6,7 @@ const SideSnack = () => {
     const { snackOpen, setSnackOpen } = useContext(DataContext);
     return (
         <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={snackOpen} onClose={() => setSnackOpen(false)} slots={{ transition: Grow }} key={Grow} autoHideDuration={2000} >
-            <Box onClose={() => setSnackOpen(false)} sx={{ width: '100%', bgcolor: 'rgba(255, 255, 255, 0.9)', px: 2, py: 1, borderRadius: 2 }}>
+            <Box onClose={() => setSnackOpen(false)} sx={{ width: '100%', bgcolor: '#fff', px: 2, py: 1, borderRadius: 2 }}>
                 <Typography variant='subtitle2'>I'm A Victom Of My Own Success!</Typography>
             </Box>
         </Snackbar>
