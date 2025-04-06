08fdf8f55684175aa7373608df57c502d7cdca73
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-06 17:53:45 +0330

    typing animation fixed

--- src/helpers/Typing.js
+++ src/helpers/Typing.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 
-const Typing = ({ strings = [''], typeSpeed = 0, backSpeed = 0, backDelay = 0, loop = false, showCursor = false, className = '', style = {} }) => {
+const Typing = ({ strings = [''], typeSpeed = 0, backSpeed = 0, backDelay = 0, loop = false, showCursor = false, className = null, style = {} }) => {
     const typeEl = useRef(null);
     useEffect(() => {
         const animation = new Typed(typeEl.current, { strings, typeSpeed, backSpeed, backDelay, loop, showCursor })
