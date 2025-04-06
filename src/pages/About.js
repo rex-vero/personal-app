6f45a1d76bd017268df0b444d144f80cb5beacbe
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-06 20:33:22 +0330

    about page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Box, Grid, IconButton, Tooltip, Typography, Zoom } from "@mui/material";
+import { Box, Divider, Grid, IconButton, Stack, Tooltip, Typography, Zoom } from "@mui/material";
 import ParticlesContainer from "../helpers/ParticlesContainer";
 import { green } from "@mui/material/colors";
 import { icons } from "../helpers/tooltip";
@@ -6,18 +6,29 @@ import Typing from "../helpers/Typing";
 
 const About = () => {
     return (
-        <Box>
+        <Box p={4}>
             <ParticlesContainer particleColor={green[900]} animationType={'number'} />
-            <Typography variant="h5" color="#fff">
-                <Typing strings={['Things I Know:']} typeSpeed={200} showCursor />
-            </Typography>
-            <Grid container>
-                {icons.map((item, key) => (
-                    <Tooltip key={key} title={item.title} arrow slots={{ transition: Zoom }} slotProps={{ transition: { timeout: 300 } }}>
-                        <IconButton sx={{ transition: '.5s', '&.MuiIconButton-root:hover': { color: item.color } }}>{item.icon}</IconButton>
-                    </Tooltip>
-                ))}
-            </Grid>
+            <Stack width={{ xs: '90vw', md: '30vw' }}>
+                <Typography variant="body1" color="#fff">
+                    <Typing strings={["I'm a passionate Front-End Developer with strong expertise in React, Material-UI, Sass, HTML, CSS, Git, Bootstrap, and JavaScript. I build modern, responsive, and user-friendly interfaces that bring ideas to life. I'm always open to learning new technologies and pushing my limits because in the world of code, growth never stops."]} typeSpeed={20} />
+                </Typography>
+                <Divider sx={{ mt: 5 }} />
+                <Typography mt={5} variant="body1" color="#fff">
+                    <Typing strings={["“Every line of code I write is not just syntax, it's a step toward creating something meaningful.”"]} typeSpeed={60} />
+                </Typography>
+            </Stack>
+            <Box mt={6}>
+                <Typography variant="h5" color="#fff">
+                    <Typing strings={['Things I Know:']} typeSpeed={200} showCursor />
+                </Typography>
+                <Grid container>
+                    {icons.map((item, key) => (
+                        <Tooltip key={key} title={item.title} arrow slots={{ transition: Zoom }} slotProps={{ transition: { timeout: 300 } }}>
+                            <IconButton sx={{ transition: '.5s', '&.MuiIconButton-root:hover': { color: item.color } }}>{item.icon}</IconButton>
+                        </Tooltip>
+                    ))}
+                </Grid>
+            </Box>
         </Box>
     );
 }
