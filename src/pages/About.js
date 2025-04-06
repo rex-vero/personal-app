f524c9117ddfa7e6775a735cb6dda6e64d221291
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-06 21:12:55 +0330

    about page fixed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Box, Divider, Grid, IconButton, Stack, Tooltip, Typography, Zoom } from "@mui/material";
+import { Box, Chip, Divider, Grid, IconButton, Stack, Tooltip, Typography, Zoom } from "@mui/material";
 import ParticlesContainer from "../helpers/ParticlesContainer";
 import { green } from "@mui/material/colors";
 import { icons } from "../helpers/tooltip";
@@ -8,7 +8,7 @@ const About = () => {
     return (
         <Box p={1}>
             <ParticlesContainer particleColor={green[900]} animationType={'number'} />
-            <Grid sx={{ flexDirection: 'column' }} container>
+            <Grid sx={{ flexDirection: 'column', width: { xs: 'auto', md: '30vw' } }} container>
                 <Typography variant="body1" color="#fff">
                     <Typing strings={["I'm a passionate Front-End Developer with strong expertise in React, Material-UI, Sass, HTML, CSS, Git, Bootstrap, and JavaScript. I build modern, responsive, and user-friendly interfaces that bring ideas to life. I'm always open to learning new technologies and pushing my limits because in the world of code, growth never stops."]} typeSpeed={5} />
                 </Typography>
@@ -18,9 +18,7 @@ const About = () => {
                 </Typography>
             </Grid>
             <Box mt={6}>
-                <Typography variant="h5" color="#fff">
-                    <Typing strings={['Things I Know:']} typeSpeed={200} showCursor />
-                </Typography>
+                <Chip label={<Typing strings={['Things I Know:']} typeSpeed={200} showCursor />} />
                 <Grid container>
                     {icons.map((item, key) => (
                         <Tooltip key={key} title={item.title} arrow slots={{ transition: Zoom }} slotProps={{ transition: { timeout: 300 } }}>
