42531ea355ddea0b6074429ba28fda12496d4356
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-04-19 22:58:32 +0330

    Ui updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import { Box, IconButton, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Tooltip, Typography, Zoom } from "@mui/material";
 import ParticlesContainer from "../helpers/ParticlesContainer";
 import { lightBlue } from "@mui/material/colors";
 import { useContext, useLayoutEffect } from "react";
@@ -7,7 +7,6 @@ import { translations } from "../utils/translations";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import { Link } from "react-router-dom";
 import { contacts } from "../utils/contacts";
-import { floatAnimation } from "../utils/animations";
 
 const Contact = () => {
     const { lang, themeMode, verify, setVerify } = useContext(DataContext);
@@ -25,16 +24,18 @@ const Contact = () => {
         <>
             <ParticlesContainer background={themeMode === 'dark' ? '#080808' : 'rgb(185, 185, 185)'} animationType={'color'} particleColor={lightBlue[700]} />
             {!verify ? <HCaptcha languageOverride={lang} theme={themeMode} sitekey="56307bd4-fc66-4a5e-8110-c65605d0d14a" onVerify={handleVerify} /> : (
-                <Box p={3} borderRadius={3} bgcolor={'rgba(214, 214, 214, 0.18)'} boxShadow={themeMode === 'dark' ? '0 0 10px white' : '0 0 10px black'} justifyItems={'center'} sx={{ backdropFilter: 'blur(5px)', animation: `${floatAnimation} 2s ease-in-out infinite` }}>
+                <Box p={3} borderRadius={3} bgcolor={'rgba(214, 214, 214, 0.18)'} boxShadow={themeMode === 'dark' ? '0 0 10px white' : '0 0 10px black'} justifyItems={'center'} sx={{ backdropFilter: 'blur(5px)' }}>
                     <Stack alignItems={'center'}>
                         <Typography color="text.primary" variant="h5" textAlign={'center'}>{contactText}</Typography>
                         <Box>
                             {contacts.map((item, key) => (
-                                <Link key={key} to={item.href} target="_blank">
-                                    <IconButton sx={{ fontSize: 40, color: 'text.primary', transition: '.5s', '&.MuiIconButton-root:hover': { color: item.color } }}>
-                                        {item.icon}
-                                    </IconButton>
-                                </Link>
+                                <Tooltip key={key} title={item.title} arrow slots={{ transition: Zoom }} slotProps={{ transition: { timeout: 300 } }}>
+                                    <Link to={item.href} target="_blank">
+                                        <IconButton sx={{ fontSize: 40, color: 'text.primary', transition: '.5s', '&.MuiIconButton-root:hover': { color: item.color } }}>
+                                            {item.icon}
+                                        </IconButton>
+                                    </Link>
+                                </Tooltip>
                             ))}
                         </Box>
                     </Stack>
--- src/utils/contacts.js
+++ src/utils/contacts.js
@@ -2,8 +2,8 @@ import { FaGithub, FaLinkedin, FaTelegram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 
 export const contacts = [
-    { href: 'https://t.me/rex_vero', color: '#28a8e8', icon: <FaTelegram /> },
-    { href: 'https://github.com/rex-vero', color: '#7b2fa0', icon: <FaGithub /> },
-    { href: 'https://www.linkedin.com/in/ahmadi-mohammad', color: '#0066c8', icon: <FaLinkedin /> },
-    { href: 'mailto:ahmadi.smohammadamin@gmail.com', color: '#eb4132', icon: <SiGmail /> }
+    { title: 'Telegram', href: 'https://t.me/rex_vero', color: '#28a8e8', icon: <FaTelegram /> },
+    { title: 'Github', href: 'https://github.com/rex-vero', color: '#7b2fa0', icon: <FaGithub /> },
+    { title: 'Linkedin', href: 'https://www.linkedin.com/in/ahmadi-mohammad', color: '#0066c8', icon: <FaLinkedin /> },
+    { title: 'Gmail', href: 'mailto:ahmadi.smohammadamin@gmail.com', color: '#eb4132', icon: <SiGmail /> }
 ];
